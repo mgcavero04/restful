5a99e5904a3a11af333fb29eba38a3af5cf31ce2
Martha Garcia <mgcavero04@hotmail.com>
2016-09-29 21:58:09 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -11,50 +11,16 @@ var birds = require('./birds');
 app.use('/birds', birds);
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-
-
-var bookRouter=express.Router();
+var bookRouter=require('./Routes/bookRoutes')(Book);
+app.use('/api/books', bookRouter);
 app.get('/', function(req, res) {
     res.send('Welcome to my API');
-    
-});
-app.use('/api', bookRouter);
-bookRouter.route('/Books')
-.post(function(req, res)
-{
-    var book= new Book(req.body);
-    book.save();
-    res.status(201).send(book)
-})
-.get(function(req,res){
-    var query={};
-    if(req.query.genre)
-    {
-        query.genre=req.query.genre;
-    }
-    Book.find(query, function(err, books){
-
-        assert.equal(null, err);
-  
-        if(err)
-            res.status(500).send(err);
-        else
-            console.log("Connected successfully to server");
-            res.json(books);
-        });       
     });
-bookRouter.route('/Books/:bookId').get(function(req,res){
-    Book.findById(req.params.bookId, function(err, book){
 
-        assert.equal(null, err);
-  
-        if(err)
-            res.status(500).send(err);
-        else
-            console.log("Connected successfully to server");
-            res.json(book);
-        });       
-    });
+
+
+
+
 
 
 app.listen(port, function(){
